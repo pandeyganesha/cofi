a3e14899b4239b26fcfb8d24d896ab42a875937d
pandeyganesha <pandeyganesha@gmail.com>
2026-09-10 10:18:28 +0530

    Implement application launcher with GTK4 integration

---
+++ src/display.rs
@@ -0,0 +1,25 @@
+use gtk4::{ApplicationWindow, prelude::*};
+use gtk4::{Application, glib};
+
+
+const APP_ID: &str = "dev.pandey.cofi";
+
+pub fn scatter_on_screen(app_names: &[String]) -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    for app_name in app_names {
+        println!("{app_name}");
+    }
+
+    app.connect_activate(build_ui);
+    app.run()
+}
+
+fn build_ui(app: &Application){
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My App")
+        .build();
+
+    window.present();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod display;
+
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let mut app_names: Vec<String> = Vec::new();
+
+    for entry in fs::read_dir("/usr/share/applications")? {
+        let entry = entry ?;
+        let path = entry.path();
+
+        if path.is_file(){
+            if let Some(name) = path.file_name(){
+                app_names.push(name.to_string_lossy().to_string());
+            } 
+        }
+    }
+    display::scatter_on_screen(&app_names);
+    Ok(())
 }
