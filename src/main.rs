0e2f0eba796754e83b691d775ee37b69f7e9a2db
pandeyganesha <pandeyganesha@gmail.com>
2026-09-18 21:48:47 +0530

    Add freedesktop_entry_parser dependency and implement show_on_ui function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 mod display;
 
 use std::fs;
+use std::path::Path;
+use freedesktop_entry_parser::parse_entry;
+
+fn show_on_ui(path: &String) -> bool {
+    let entry = match parse_entry(path){
+        Ok(entry) => entry,
+        Err(_) => return false,
+    };
+
+    let value = entry
+    .section("Desktop Entry")
+    .expect("Did not find Desktop Entry")
+    .attr("NoDisplay")
+    .get(0)
+    .expect("No Display exist")
+    .to_lowercase();
+
+    match value.as_str() {
+        "true" => true,
+        _ => false
+    }
+}
 
 fn main() -> std::io::Result<()> {
     let mut app_names: Vec<String> = Vec::new();
@@ -15,6 +37,11 @@ fn main() -> std::io::Result<()> {
             } 
         }
     }
+    // let filtered_apps: Vec<String> = app_names
+    // .iter()
+    // .filter(|app| show_on_ui(app))
+    // .collect();
+
     display::scatter_on_screen(app_names);
     Ok(())
 }
